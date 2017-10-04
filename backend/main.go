735c246fa933b8321d5ec188906031ed9f914dde
James Carlson <carlso70@purdue.edu>
2017-10-04 12:49:38 -0400

    Add Tcp

--- backend/main.go
+++ backend/main.go
@@ -1,16 +1,25 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
 
+	"github.com/carlso70/triviacast/backend/gameserver"
 	"github.com/carlso70/triviacast/backend/routing"
 )
 
 func main() {
 	fmt.Println("Launching Server")
 
+	// GetInstance inits the gamemanager singleton, and the TCP socket server
+	gameserver.GetInstance()
+	if gameserver.Listener != nil {
+		defer gameserver.Listener.Close()
+	} else {
+		panic(errors.New("Listener NOT STARTED"))
+	}
 	router := routing.NewRouter()
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
