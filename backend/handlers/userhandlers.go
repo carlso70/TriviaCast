4f2d504a1da9d7e0ecd158431cce38831f62708a
Ubuntu <ubuntu@ip-172-31-26-90.us-east-2.compute.internal>
2017-10-06 18:03:36 +0000

    Fix print out in handlers

--- backend/handlers/userhandlers.go
+++ backend/handlers/userhandlers.go
@@ -42,7 +42,7 @@ func CreateUser(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Fprint(w, "%s", string(byteSlice))
+	fmt.Fprintf(w, "%s", string(byteSlice))
 }
 
 // RequestUsers gets a list of all the users
@@ -59,6 +59,6 @@ func ListUsers(w http.ResponseWriter, r *http.Request) {
 		if err != nil {
 			panic(err)
 		}
-		fmt.Fprint(w, "%s\n", string(byteSlice))
+		fmt.Fprintf(w, "%s\n", string(byteSlice))
 	}
 }
