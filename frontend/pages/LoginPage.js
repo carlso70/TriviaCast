15b89325f13fec57f7d6b46850f21000e192a11b
James Carlson <carlso70@purdue.edu>
2017-10-31 14:19:18 -0400

    Some login code

--- frontend/pages/LoginPage.js
+++ frontend/pages/LoginPage.js
@@ -26,7 +26,21 @@ export default class LoginPage extends React.Component {
 
     authenticate() {
         // TODO make post request to server, if successful run the nav code below, and pass isLogin param
-        this.props.navigation.goBack();
+        fetch('ec2-18-221-200-72.us-east-2.compute.amazonaws.com:8080/loginuser',{
+              method: 'POST',
+              headers: {
+                  'Accept': 'application/json',
+                  'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({
+                  username: this.state.username,
+                  password: this.state.password,
+              })
+        }).then((response) => response.json())
+            .then((responseJson) => {
+                console.log(responseJson);
+                this.props.navigation.goBack();
+            })
     }
     createAccount() {
         this.props.navigation.goBack();
