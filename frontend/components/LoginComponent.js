e852ba253387c847ef20029d62789e8fa3d25272
Ram <ramdent2@gmail.com>
2017-11-02 15:13:33 -0400

    splash screen button

--- frontend/components/LoginComponent.js
+++ frontend/components/LoginComponent.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {View, Text} from 'react-native'
+import {View, Text, StyleSheet} from 'react-native'
 import { Button } from 'react-native-elements'
 
 import { StackNavigator, NavigationActions } from 'react-navigation';
@@ -22,8 +22,19 @@ export default class LoginComponent extends React.Component {
                 >
                 {'TriviaCast'}
             </Text>
-                <Button title="Login" onPress={() => this.props.navigation.navigate('Login')}/>
+                <Button buttonStyle={styles.buttons} color="black" title="Login" onPress={() => this.props.navigation.navigate('Login')}/>
                 </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    buttons: {
+        alignItems: 'center',
+        padding: 20,
+        backgroundColor: 'white',
+        marginTop: 20,
+        borderRadius: 10
+
+    }
+})
