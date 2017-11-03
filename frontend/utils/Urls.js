74ffff3e57781d6c23763d64c35be3a14891231b
James Carlson <carlso70@purdue.edu>
2017-11-03 03:02:27 -0400

    Sockets sent from phone

--- frontend/utils/Urls.js
+++ frontend/utils/Urls.js
@@ -2,3 +2,6 @@ export const getAWSUrl = () => {
     return 'http://ec2-18-221-200-72.us-east-2.compute.amazonaws.com:8080/';
 }
 
+export const getWebsocketUrl = () => {
+    return 'http://ec2-18-221-200-72.us-east-2.compute.amazonaws.com:3000/';
+}
